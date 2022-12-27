6ffabf7763169beb08ed69a82f754c619d237099
Ivan <aleksanivan@gmail.com>
2022-12-27 14:55:22 +0300

    added news sort

--- components/shared/Gallery/Gallery.vue
+++ components/shared/Gallery/Gallery.vue
@@ -2,18 +2,9 @@
   <div class="carousel-wrapper" id="spinnerBlock">
     <client-only>
       <swiper class="swiper" :options="swiperOption" @ready="onSwiperRedied">
-        <swiper-slide
-          class="slider-item d-project-item"
-          v-for="(item, index) in news"
-          :key="index"
-        >
-          <Gallery_item
-            :id="item.id"
-            :img="item.background_image.url"
-            :title="item.short_title"
-            :date="item.date ? item.date : item.published_at"
-            :tags="item.tags_news"
-          />
+        <swiper-slide class="slider-item d-project-item" v-for="(item, index) in news" :key="index">
+          <Gallery_item :id="item.id" :img="item.background_image.url" :title="item.short_title"
+            :date="item.date ? item.date : item.published_at" :tags="item.tags_news" />
         </swiper-slide>
       </swiper>
     </client-only>
@@ -21,58 +12,75 @@
 </template>
 
 <script>
-  import Gallery_item from './Gallery_item';
+import Gallery_item from './Gallery_item';
 
-  export default {
-    name: 'Gallery',
-    components: { Gallery_item },
+export default {
+  name: 'Gallery',
+  components: { Gallery_item },
 
-    async fetch() {
-      this.news = await fetch(
-        'https://strapi.uparchitecture.nl/news-lists',
-      ).then(res => res.json());
-    },
+  async fetch() {
+    const results = await fetch(
+      'https://strapi.uparchitecture.nl/news-lists',
+    ).then(res => res.json());
+    results.sort(function (a, b) {
+      // Convert the date strings to dates
+      const dateA = new Date(a.date);
+      const dateB = new Date(b.date);
 
-    data() {
-      return {
-        news: {},
-        swiperOption: {
-          slidesPerView: 'auto',
-          spaceBetween: 30,
-          loop: true,
-          loopedSlides: 5,
-          freeMode: true,
-          centeredSlides: true,
-        },
-      };
-    },
-    methods: {
-      onSwiperRedied(swiper) {
-        setTimeout(function() {
-          swiper.slideReset();
-        }, 500);
+      // Compare the dates and return a value based on the comparison
+      if (dateA > dateB) {
+        return -1;
+      } else if (dateA < dateB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    const lastElements = results.splice(results.length - 2, 2);
+    results.unshift(...lastElements);
+    this.news = results;
+  },
+
+  data() {
+    return {
+      news: {},
+      swiperOption: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        loop: true,
+        loopedSlides: 5,
+        freeMode: true,
+        centeredSlides: true,
       },
+    };
+  },
+  methods: {
+    onSwiperRedied(swiper) {
+      setTimeout(function () {
+        swiper.slideReset();
+      }, 500);
     },
-  };
+  },
+};
 </script>
 
 <style scoped>
-  .carousel-wrapper {
-    width: 100%;
-    overflow: hidden;
-    cursor: none;
-  }
+.carousel-wrapper {
+  width: 100%;
+  overflow: hidden;
+  cursor: none;
+}
 
-  .img-wrapper {
-    min-height: 300px;
-    outline: 0;
-  }
+.img-wrapper {
+  min-height: 300px;
+  outline: 0;
+}
 
-  .img-wrapper img {
-    pointer-events: none;
-  }
+.img-wrapper img {
+  pointer-events: none;
+}
 
-  .slider-item {
-    width: auto;
-  }
+.slider-item {
+  width: auto;
+}
 </style>
